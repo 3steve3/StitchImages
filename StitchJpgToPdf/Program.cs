848d9a9427d5c4bddb1b8e37f11826bbf2b08ce3
Steve <stna7972@colorado.edu>
2023-01-19 14:43:34 -0700

    Add project files.

---
+++ StitchJpgToPdf/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+int NumberOfPages = 308;
+string InputDirectory = "C:\\Users\\Steve\\Downloads\\1111";
+string OutputFile = "C:\\Users\\Steve\\Desktop\\textbook.pdf";
+PdfWriter writer = new PdfWriter(OutputFile);
+PdfDocument pdf = new PdfDocument(writer);
+pdf.SetDefaultPageSize(new iText.Kernel.Geom.PageSize(1224, 1566));
+Document doc = new Document(pdf);
+doc.SetBottomMargin(0);
+doc.SetTopMargin(0);
+doc.SetRightMargin(0);
+doc.SetLeftMargin(0);
+for(int i = 0; i < NumberOfPages - 1; i++)
+{
+    ImageData data = ImageDataFactory.Create(InputDirectory + $"\\Page {i}.jpg");
+    Image img = new Image(data);
+    doc.Add(img.ScaleToFit(1224, 1566));
+    
+}
+ImageData LastPage = ImageDataFactory.Create(InputDirectory + $"\\Page {NumberOfPages-1}.jpg");
+doc.Add(new Image(LastPage));
+doc.Close();
